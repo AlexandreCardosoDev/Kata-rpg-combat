4877f05cd185fd5b0f51a5b73e6e84ab5c908a8f
Alexandre Cardoso <alexmcardoso@gmail.com>
2024-08-29 17:30:14 -0300

    add test and logical about  Iteration One

--- src/char.js
+++ src/char.js
@@ -1,9 +1,22 @@
 class Char {
   
+
   constructor () {
     this.health = 1000
     this.level = 1
-    this.isAlive = true
+  }
+
+  get isAlive() {
+    return this.health > 0 
+  }
+  attack(target) {
+    let damage = 100
+    target.health = Math.max(0, target.health - damage) 
+  }
+
+  heal(target) {
+    if (!target.isAlive) return
+    target.health = Math.min(1000, target.health + 100) 
   }
 }
 
--- tests/char.test.js
+++ tests/char.test.js
@@ -16,6 +16,48 @@ describe('Char', () => {
     })
   })
 
- 
+  describe('Characters can Deal Damage to Characters', () => {
+    it('Damage is subtracted from Health', () => {
+      let charA = new Char()
+      let charB = new Char()
+
+      charA.attack(charB)
+
+      expect(charB.health).toBe(900)
+    })
+    it('When damage received exceeds current Health, Health becomes 0 and the character dies', () => {
+      let charA = new Char()
+      let charB = new Char()
+
+      repeat(() => charA.attack(charB), 10)
+
+      expect(charB.health).toBe(0)
+      expect(charB.isAlive).toBe(false)
+    })
+
+    describe('A Character can Heal a Character', () => {
+      it('Dead characters cannot be healed', () => {
+        let charA = new Char()
+        let charB = new Char()
+        repeat(() => charA.attack(charB), 10)
+        charA.heal(charB)
+        expect(charB.health).toBe(0)
+        expect(charB.isAlive).toBe(false)
+      })
+      it('Healing cannot raise health above 1000', () => {
+        let charA = new Char()
+        let charB = new Char()
+        repeat(() => charA.heal(charB), 10)
+        expect(charB.health).toBe(1000)
+      })
+    })
+    
+  })
+
+  function repeat(action, times) {
+    for (let i = 0; i < times; i++) {
+      action()      
+    }
+  }
 
 })
