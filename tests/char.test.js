d56d57004a53ea43a654306c885dc031497b02f1
Alexandre Cardoso <alexmcardoso@gmail.com>
2024-08-29 17:35:04 -0300

    refactoring tests Interation One

--- tests/char.test.js
+++ tests/char.test.js
@@ -1,34 +1,34 @@
 const Char = require('../src/char')
 
 describe('Char', () => {
+  let charA
+  let charB
+  
+  beforeEach(() => {
+    charA = new Char()
+    charB = new Char()
+  })
+
   describe('All Characters when created, have ...', () => {
     it('Health, starting at 1000', () => {
-      let char = new Char()
-      expect(char.health).toBe(1000)
+      expect(charA.health).toBe(1000)
     })
     it('Level, starting at 1', () => {
       let char = new Char()
-      expect(char.level).toBe(1)
+      expect(charA.level).toBe(1)
     })
     it('starting status Alive', () => {
-      let char = new Char()
-      expect(char.isAlive).toBe(true)
+      expect(charA.isAlive).toBe(true)
     })
   })
 
   describe('Characters can Deal Damage to Characters', () => {
     it('Damage is subtracted from Health', () => {
-      let charA = new Char()
-      let charB = new Char()
-
       charA.attack(charB)
 
       expect(charB.health).toBe(900)
     })
     it('When damage received exceeds current Health, Health becomes 0 and the character dies', () => {
-      let charA = new Char()
-      let charB = new Char()
-
       repeat(() => charA.attack(charB), 10)
 
       expect(charB.health).toBe(0)
@@ -37,16 +37,12 @@ describe('Char', () => {
 
     describe('A Character can Heal a Character', () => {
       it('Dead characters cannot be healed', () => {
-        let charA = new Char()
-        let charB = new Char()
         repeat(() => charA.attack(charB), 10)
         charA.heal(charB)
         expect(charB.health).toBe(0)
         expect(charB.isAlive).toBe(false)
       })
       it('Healing cannot raise health above 1000', () => {
-        let charA = new Char()
-        let charB = new Char()
         repeat(() => charA.heal(charB), 10)
         expect(charB.health).toBe(1000)
       })
